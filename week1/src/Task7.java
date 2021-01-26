20877c2a777f95e122c37424f4bfd3864f0c9692
Maruko-Lv <603861496@qq.com>
2021-01-26 16:34:56 +0800

    第七题：用自定义的类作为数据类型，类似于c语言的结构体。学习到了怎么在一行输入两个数字（用String数组以及String类中的split方法），还有自己重写了toString方法以实现实数形式的输出（要不然会输出地址值）

--- week1/src/Task7.java
+++ week1/src/Task7.java
@@ -2,9 +2,65 @@
  * @author LXX
  * @date 2021/1/23-21:38
  */
-public class Task7 {
 
+import java.util.Scanner;
+
+/**
+ * 声明并测试一个复数类，其中包括 add()、reduce()、multiply()三种方法，分别实现两
+ * 个复数的相加、相减、相乘，然后输出计算的结果。
+ */
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] input1 = in.nextLine().split(" ");
+        String[] input2 = in.nextLine().split(" ");
+        ComplexNumber complexNumber1 = new ComplexNumber(Integer.parseInt(input1[0]), Integer.parseInt(input1[1]));
+        ComplexNumber complexNumber2= new ComplexNumber(Integer.parseInt(input2[0]), Integer.parseInt(input2[1]));
+        System.out.println("两复数相加的结果为：" + complexNumber1.add(complexNumber2));
+        System.out.println("两复数相减的结果为：" + complexNumber1.reduce(complexNumber2));
+        System.out.println("两复数相乘的结果为：" + complexNumber1.multiply(complexNumber2));
+    }
 }
 class ComplexNumber{
-    
+    private int x;//实部
+    private int y;//虚部
+
+    public ComplexNumber(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public ComplexNumber add(ComplexNumber complexNumber){
+        return new ComplexNumber(x + complexNumber.getX(),y + complexNumber.getY());
+    }
+
+    public ComplexNumber reduce(ComplexNumber complexNumber){
+        return new ComplexNumber(x - complexNumber.getX(),y - complexNumber.getY());
+    }
+
+    public ComplexNumber multiply(ComplexNumber complexNumber){
+        return new ComplexNumber((x * complexNumber.getX()) - (y * complexNumber.getY()),
+                (y * complexNumber.getX()) + (x * complexNumber.getY()));
+    }
+
+    @Override
+    public String toString() {
+        return x + "+" + y +"i";
+    }
 }
